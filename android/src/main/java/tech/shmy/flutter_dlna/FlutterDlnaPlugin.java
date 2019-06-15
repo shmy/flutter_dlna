0571a5eb050095a8d5e4f7deaafa40d919eed6c1
shmy <pandahao123>
2019-06-15 15:38:29 +0800

    updated

--- android/src/main/java/tech/shmy/flutter_dlna/FlutterDlnaPlugin.java
+++ android/src/main/java/tech/shmy/flutter_dlna/FlutterDlnaPlugin.java
@@ -60,7 +60,7 @@ public class FlutterDlnaPlugin implements MethodCallHandler, EventChannel.Stream
       result.notImplemented();
     }
   }
-  private List<HashMap> getDevicesList() {
+  private ArrayList<HashMap> getDevicesList() {
     List<Device> devices = DLNAContainer.getInstance().getDevices();
     for (Device device1 : devices) {
       HashMap hm = new HashMap();
